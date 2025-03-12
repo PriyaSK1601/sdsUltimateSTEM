a95ffbcb0e371183452451f3c025f969f779084b
ju1iapham <julia.pham.n@gmail.com>
2025-03-12 12:34:21 +1100

    Updated Navbar.js and Navbar.css
    Added assets folder and updated App.js, App.css, and index.js

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Logo from "../assets/logo.png";
+import ProfileIcon from "../assets/profile_icon.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={Logo} alt="Lightbulb logo" />
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/dashboard"> Dashboard </Link>
+        <Link to="/tournament"> Tournament </Link>
+        <Link to="/submission"> Submission </Link>
+        <Link to="/profile">
+          <img src={ProfileIcon} alt="Profile Icon" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
