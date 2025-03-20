43bf7c3fc0b34257435de0d67bfacbaeaff3c5e1
PriyaSK1601 <priya.sji1234@gmail.com>
2025-03-20 17:37:04 +1100

    Update login.js

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -15,9 +15,7 @@ function Login() {
       await signInWithEmailAndPassword(auth, email, password);
       console.log("User logged in Successfully");
       toast.success("User logged in Successfully", { position: "top-center" });
-      setTimeout(() => {
-        window.location.href = "/profile";
-      }, 1500);
+      window.location.href = "/profile";
     } catch (error) {
       console.log(error.code, error.message);
 
