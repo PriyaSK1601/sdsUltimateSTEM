0bc9c93c6c9c498354e5c815cc3ae9eabfd60fcc
ju1iapham <julia.pham.n@gmail.com>
2025-04-02 10:48:54 +1100

    Updated Home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,10 @@ import Navbar from "./components/Navbar";
 
 import Profile from "./pages/profile";
 import Login from "./pages/login";
-import SignUp from "./pages/register"; 
+import SignUp from "./pages/register";
 import Home from "./pages/Home";
 import Submission from "./pages/Submission";
-import Tournament from "./pages/Tournament"; 
+import Tournament from "./pages/Tournament";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { useEffect } from "react";
@@ -20,39 +20,51 @@ import { Navigate } from "react-router-dom";
 //Changed
 
 function App() {
-
   const [user, setUser] = useState();
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
     });
   });
-  
+
   //Store submissions here
   const [submissions, setSubmissions] = useState([]);
   const handleNewSubmission = (submissionData) => {
     setSubmissions((prev) => [...prev, submissionData]);
-  }
-  
+  };
+
   return (
     <div className="App">
-
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Router>
             <Navbar />
             <Routes>
-              <Route path="/" exact element={<Home />} />
               <Route
-                    path="/"
-                    element={user ? <Navigate to="/profile" /> : <Login />}
+                path="/"
+                exact
+                element={<Home submissions={submissions} />}
               />
-              <Route path="/home" exact element={<Home />} />
+              <Route
+                path="/"
+                element={user ? <Navigate to="/profile" /> : <Login />}
+              />
+              <Route
+                path="/home"
+                exact
+                element={<Home submissions={submissions} />}
+              />
               <Route path="/profile" element={<Profile />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<SignUp />} />
-              <Route path="/submission" element={<Submission onSubmit={handleNewSubmission} />} />
-              <Route path="/tournament" element={<Tournament submissions={submissions}/>}/>
+              <Route
+                path="/submission"
+                element={<Submission onSubmit={handleNewSubmission} />}
+              />
+              <Route
+                path="/tournament"
+                element={<Tournament submissions={submissions} />}
+              />
             </Routes>
           </Router>
         </div>
@@ -62,4 +74,3 @@ function App() {
 }
 
 export default App;
-
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,38 +1,49 @@
 import React from "react";
 import "../styles/Home.css";
 import { Link } from "react-router-dom";
-import Book from "../assets/bookIdea.png";
-import background from "../assets/home.webp";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Books from "../assets/books.png";
 import Podium from "../components/Podium";
 import Footer from "../components/Footer";
 
-function Home() {
+function Home({ submissions = [] }) {
   return (
-    <div
-      className="home"
-      style={{
-        backgroundImage: `URL(${background})`,
-      }}
-    >
-      <div className="circle">
-        <img src={Book} alt="Book with lightbulb" />
+    <div className="container-fluid p-0 home-background">
+      <div className="row h-100 justify-content-center pt-5">
+        <div className="col-md-6 mt-5">
+          <div className="bg-secondary bg-opacity-25 p-5 mt-5 header-container">
+            <h1 className="text-center mb-5">Join the Tournament!</h1>
+            <p className="mx-4 mb-5 header-paragraph">
+              Got an idea for a STEM topic that could be turned into a
+              children's book? Submit your idea today! Or check out our upcoming
+              and currently-running tournaments.
+            </p>
+            <div className="justify-content-left mx-4 button-container">
+              <Link to="/tournament">
+                <button className="btn btn-secondary button-one">
+                  View Tournament
+                </button>
+              </Link>
+              <Link to="/submission">
+                <button className="btn btn-secondary button-two">
+                  Submit Idea
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="rounded-circle bg-secondary bg-opacity-50 position-relative circle-container">
+            <img
+              src={Books}
+              alt="Chris Ferrie's Books"
+              className="position-absolute book-image"
+            />
+          </div>
+        </div>
       </div>
-      <div className="headerContainer">
-        <h1>Join the tournament!</h1>
-        <p>
-          Got an idea? Submit your idea today! Or check out our upcoming and
-          currently-running tournaments.
-        </p>
-        <Link to="/tournament">
-          <button className="headerContainerButton1"> View Tournament </button>
-        </Link>
-        <Link to="/submission">
-          <button className="headerContainerButton2"> Submit Idea </button>
-        </Link>
-      </div>
-      <Podium />
+      <Podium submissions={submissions} />
       <Footer />
     </div>
   );
 }
+
 export default Home;
