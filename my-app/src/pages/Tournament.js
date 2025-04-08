390481dba1f710e6101c8a2bf4f530448fa341ca
alazarevic2 <anna.lazarevic@student.uts.edu.au>
2025-04-08 17:44:43 +1000

    Fixed up the submission cards to be used in the homepage

--- my-app/src/pages/Tournament.js
+++ my-app/src/pages/Tournament.js
@@ -10,24 +10,23 @@ function Tournament({ submissions }) {
             ) : (
                 <div className="submissions-list">
                     {submissions.map((submission, index) => (
-                        <div key={index} className="card">
-                            <div
-                                className="card-image"
-                                style={{
-                                    backgroundImage: submission.image
-                                        ? `url(${submission.image})`
-                                        : "none",
-                                }}
-                            ></div>
-                            <div className="card-content">
-                                <div className="category">{submission.category}</div>
-                                <div className="heading">{submission.title}</div>
-                                <div className="description">{submission.description}</div>
-                                <div className="author">
-                                    By <span className="name">{submission.author}</span> {submission.date}
-                                </div>
+                        <div className="card">
+                        <div
+                            className="card-image"
+                            style={{
+                                backgroundImage: submission.image ? `url(${submission.image})` : "none",
+                            }}
+                        ></div>
+                        <div className="card-content">
+                            <div className="category">{submission.category}</div>
+                            <div className="heading">{submission.title}</div>
+                            <div className="description">{submission.description}</div>
+                            <div className="author">
+                                By <span className="name">{submission.author}</span> {submission.date}
                             </div>
                         </div>
+                    </div>
+                    
                     ))}
                 </div>
             )}
