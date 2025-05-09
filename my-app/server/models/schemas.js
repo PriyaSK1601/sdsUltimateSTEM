265e858f5c70f5a8aded40aeddef4218f6c3fd73
alazarevic2 <anna.lazarevic@student.uts.edu.au>
2025-05-09 11:20:45 +1000

    torunament schema added

--- my-app/server/models/schemas.js
+++ my-app/server/models/schemas.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//The data that is saved in the databases on MONGO:
+
+//Submission Schema
 const submissionSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
@@ -12,8 +15,19 @@ const submissionSchema = new Schema({
   },
   entryDate: { type: Date, default: Date.now },
   status: { type: String, default: "pending", enum: ["pending", "approved", "declined"] },
+  votes: { type: Number, default: 0 },
 });
 
 const Submission = mongoose.model("Submission", submissionSchema, "submission-data");
 
-module.exports = { Submission };
+// Tournament Schema
+const tournamentSchema = new Schema({
+  round: { type: Number, required: true },
+  endDate: { type: Date, required: true },
+  submissionID: { type: Schema.Types.ObjectId, ref: 'Submission' },
+  submissionVotes: { type: Number, default: 0 },
+});
+
+const Tournament = mongoose.model("Tournament", tournamentSchema, "tournament-data");
+
+module.exports = { Submission, Tournament};
